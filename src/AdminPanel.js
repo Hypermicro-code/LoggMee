be750e202ef19495441138ec51e31821e9b830dc
Hypermicro-code <tony2mat@gmail.com>
2025-06-17 13:35:32 +0200

    Update AdminPanel.js

--- src/AdminPanel.js
+++ src/AdminPanel.js
@@ -29,13 +29,13 @@ const AdminPanel = () => {
           <QRCode value={key} size={128} />
         </div>
       )}
-    </div>
       <button
         onClick={() => navigate('/admin')}
         style={{ marginTop: '2rem', padding: '0.75rem 1.5rem', fontSize: '1rem', backgroundColor: '#ccc', border: 'none', borderRadius: '0.5rem' }}
       >
         Tilbake til Admin-panel
       </button>
+    </div>
   );
 };
 
