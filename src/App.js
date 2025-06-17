72ae3ab31206dedc8f353f934c94803cb34c3f19
Hypermicro-code <tony2mat@gmail.com>
2025-06-17 13:31:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminDashboard from './AdminDashboard';
 import AdminPanel from './AdminPanel';
+import AdminSprak from './AdminSprak';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Navigate to="/admin" />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/qr" element={<AdminPanel />} />
+        <Route path="/admin/sprak" element={<AdminSprak />} />
         <Route path="*" element={<div>404 - Ikke funnet</div>} />
       </Routes>
     </Router>
