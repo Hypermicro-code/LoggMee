afbfba79540b6335a7668909af81e840b00dae5b
Hypermicro-code <tony2mat@gmail.com>
2025-06-17 12:56:57 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js – starter appen og kobler til DOM
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
